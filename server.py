a022eb63783a97716a6a2ea969e1a09af924b2a4
PixelVolt15 <axelprot2015@gmail.com>
2026-03-21 18:15:12 -0600

    Add files via upload

--- server.py
+++ server.py
@@ -1,9 +1,13 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import requests
+import os
 
 app = Flask(__name__)
 CORS(app)
+
+@app.route("/")
+def home():
+    return "PoE AI funcionando 🚀"
 
 @app.route("/chat", methods=["POST"])
 def chat():
@@ -11,27 +15,14 @@ def chat():
         data = request.get_json()
         mensaje = data["mensaje"]
 
-        print("Mensaje recibido:", mensaje)
-
-        r = requests.post(
-            "http://127.0.0.1:11434/api/generate",
-            json={
-                "model": "llama3",
-                "prompt": mensaje,
-                "stream": False
-            }
-        )
-
-        respuesta = r.json()["response"]
-
-        print("Respuesta IA:", respuesta)
+        respuesta = f"Dijiste: {mensaje}"
 
         return jsonify({"respuesta": respuesta})
 
-    except Exception as e:
-        print("ERROR:", e)
+    except:
         return jsonify({"respuesta": "Error 😢"})
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
     
